32ca5f66655862a68e96f50767e90e1bec903197
rubendelar <delolmoruben@gmail.com>
2022-10-05 12:16:05 +0200

    done

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import { onMounted } from "vue";
+import NavBar from "./components/NavBar.vue";
+</script>
+
+<template>
+  <div class="app">
+    <NavBar />
+    <router-view />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <nav class="navbar navbar-dark bg-primary mb-3">
+      <div class="container">
+        <!-- <router-link class="navbar-brand" to="/">WIKI COUNTRIES</router-link> -->
+        <a class="navbar-brand" href="/">WIKI COUNTRIES</a>
+        <a class="text-white text-decoration-none" href="#"
+          >🚀 Build by Ruben 🚀</a
+        >
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script setup></script>
+
+<style></style>
